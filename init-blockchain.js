9ddd6e584225b2c9d3ab283b4c5de5e52ed0c27e
dev0cloo <etor.ocloo@gmail.com>
2023-02-02 12:14:26 +0000

    Edit init-blockchain.js

--- init-blockchain.js
+++ init-blockchain.js
@@ -1,10 +1,13 @@
-import { writeBlockchain } from './blockchain-helpers.js';
+import { writeBlockchain, writeTransactions } from "./blockchain-helpers.js";
 
 //first block for the blockchain
-const genesisBlock = { hash: '0', previousHash: null };
-
-// array of blocks the blockchain has
-const blockchain = [genesisBlock];
+const genesisBlock = { hash: "0", previousHash: null };
 
 // create genesis block for the blockchain
+const blockchain = [genesisBlock];
+
+// initialize the blockchain with the genesis block
 writeBlockchain(blockchain);
+
+// initialise an empty transaction pool for blockchain
+writeTransactions([]);
