68b01b259e1b658f7159a5159d7b9b4e0edb2ec8
dev0cloo <etor.ocloo@gmail.com>
2023-02-03 13:11:37 +0000

    Add generate-wallet.js

---
+++ generate-wallet.js
@@ -0,0 +1,31 @@
+import EC from "elliptic";
+import { readFileSync, writeFileSync } from "fs";
+
+// use p192 curve to create keypairs
+const ec = new EC.ec("p192");
+// generate keypair
+const keyPair = ec.genKeyPair();
+// get public key from keypair
+const publicKey = keyPair.getPublic("hex");
+// get private key from keypair
+const privateKey = keyPair.getPrivate("hex");
+
+// new wallet name defined in terminal; if left empty defaults to undefined
+const newWalletName = process.argv[2];
+
+const walletsFile = readFileSync("./wallets.json");
+let wallets = JSON.parse(walletsFile);
+
+// check if wallet name already exists
+if (!wallets.hasOwnProperty(newWalletName)) {
+  wallets[newWalletName] = publicKey;
+  wallets = JSON.stringify(wallets, null, 2);
+  writeFileSync("./wallets.json", wallets);
+  console.log(`Wallet ${newWalletName} created`);
+  console.log(`Public Key: ${publicKey} with Private Key: ${privateKey}`);
+} else {
+  console.log(`Wallet name already exists. Please use another name.`);
+  console.log(
+    `${newWalletName} exists at public address: ${wallets[newWalletName]}`
+  );
+}
